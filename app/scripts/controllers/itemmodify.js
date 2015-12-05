686f56dbb44351c28e62e98d559d5dffed255c17
yossef avital <yossef.avital@mail.hui.ac.il>
2015-12-05 17:28:26 +0200

    enum support

--- app/scripts/controllers/itemmodify.js
+++ app/scripts/controllers/itemmodify.js
@@ -8,9 +8,16 @@
  * Controller of the mngApp
  */
 angular.module('mngApp')
-	.controller('ItemmodifyCtrl', function ($scope, $interval) {
+	.controller('ItemmodifyCtrl', function ($scope, $interval, $filter) {
 		//$scope.autoSave = $interval($scope.save, 10000);
-
+		
+		$scope.showEnum = function(enumArray, enumValue) {
+			console.log(enumArray);
+			console.log(enumValue);
+			var selected = $filter('filter')(enumArray, enumValue);
+			return (enumValue && selected.length) ? selected[0] : 'Not set';
+		};
+		
 		$scope.$on('$destroy', function () {
 			//$interval.cancel($scope.autoSave);
 			$scope.save();
